d3fdc945245924a8eb70058ee4fb2c09b1e1097d
gisukahn <118027851+GisukAhn@users.noreply.github.com>
2022-11-11 16:35:04 -0800

    Frist Commit

---
+++ Gisuk_TestProject/src/Test_Package/gisuk_Class.java
@@ -0,0 +1,10 @@
+package Test_Package;
+
+public class gisuk_Class {
+
+	public static void main(String[] args) {
+		System.out.print("gisuk is the best");
+
+	}
+
+}
---
+++ Gisuk_TestProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Ki
+ *
+ */
+module Gisuk_TestProject {
+}
